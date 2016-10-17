0b01044328af17b0230ced3b3758bd8e27a31ccc
BlackEdder <edder@tkwsping.nl>
2016-10-17 18:38:54 +0100

    Rename main file

---
+++ source/easymeshed/package.d
@@ -0,0 +1,9 @@
+module easymeshed;
+
+/+ 
+Start with a minimal implementation that takes a port and starts listening. It will assume you are already connected to a node.
+
+We need to start listening and send an empty array of subconnections. It will also need to implement sync.
+
+We should also make a minimal log example program, which just logs all received messages to the command line
++/
